76396cb56404e13d6f77ce101008d5292e4dc6bf
Aniruddha Mandal <aniruddha1.2.3.3.2.1@gmail.com>
2024-01-27 21:32:16 +0530

    Added argparse for CLI

--- mailbot/inference.py
+++ mailbot/inference.py
@@ -1,12 +1,14 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import argparse
 
 class InferenceModel():
     def __init__(self, model_checkpoint: str = "distilgpt2", return_tensors='pt',
-                 return_dict_in_generate=True, num_beams=1, 
+                 return_dict_in_generate=True, output_scores=True, num_beams=1, 
                  do_sample=False, repetition_penalty=1.5, length_penalty=2.0):
         self.model_checkpoint = model_checkpoint
         self.return_tensors = return_tensors
         self.return_dict_in_generate = return_dict_in_generate
+        self.output_scores = output_scores
         self.num_beams = num_beams
         self.do_sample = do_sample
         self.repetition_penalty = repetition_penalty
@@ -38,3 +40,21 @@ class InferenceModel():
 
         generated_word = tokenizer.decode(generation_output['sequences'][0])
         return generated_word
+    
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    
+    ap.add_argument("--model_checkpoint", required=True, help="model_checkpoint")
+    ap.add_argument("--return_tensors", required=False, help="return tensors")
+    ap.add_argument("--return_dict_in_generate", required=False, help="return dict in generate")
+    ap.add_argument("--output_scores", required=False, help="output scores")
+    ap.add_argument("--num_beams", required=False, help="number of beams")
+    ap.add_argument("--do_sample", required=False, help="do sampling")
+    ap.add_argument("--repetition_penalty", required=False, help="repetition penalty")
+    ap.add_argument("--length_penalty", required=False, help="length penalty")
+    ap.add_argument("--input_text", required=True, help="input text")
+    ap.add_argument("--num_token_generate", required=False, help="number of tokens to generate")
+
+    args = vars(ap.parse_args())
+    model = InferenceModel(**args)
+    print(model.generate_text(args["input_text"], args["num_token_generate"]))
--- mailbot/trainer.py
+++ mailbot/trainer.py
@@ -3,9 +3,11 @@ import math
 from transformers import AutoModelForCausalLM, Trainer, TrainingArguments
 from dataloader import AESLCDataset
 from typing import Union, Optional
+import argparse
 
 class TrainModel():
-    def __init__(self, experiment_run_name: str, model_checkpoint: str = "distilgpt2", output_dir: str = "./model", block_size: Union[str, int] = "default",
+    def __init__(self, experiment_run_name: str, save_current_model_path: Optional[str], output_dir: str,
+                 model_checkpoint: str = "distilgpt2", block_size: Union[str, int] = "default",
                  dataloader_drop_last: bool = True, evaluation_strategy: str = "epoch", save_strategy: str = "epoch",
                  num_train_epochs: int = 10, logging_steps: int = 5, per_device_train_batch_size: int = 4,
                  per_device_eval_batch_size: int = 4, learning_rate: float = 1e-3, lr_scheduler: str = "cosine", warmup_steps: int = 10,
@@ -29,12 +31,13 @@ class TrainModel():
         self.weight_decay = weight_decay
         self.experiment_run_name = experiment_run_name
         self.monitoring_platform = monitoring_platform
+        self.save_current_model_path = save_current_model_path
 
     def load_model(self):
         model = AutoModelForCausalLM.from_pretrained(self.model_name)
         return model
     
-    def train(self):
+    def train_model(self):
         model = AutoModelForCausalLM.from_pretrained(self.model_checkpoint)
         training_args = TrainingArguments(
             output_dir=self.output_dir,
@@ -62,9 +65,39 @@ class TrainModel():
         eval_dataset=dataset["validation"],
         )
         trainer.train()
-        self.evaluate_perplexity(trainer)
+        perplexity = self.evaluate_perplexity(trainer)
+        print(f"Perplexity: {perplexity:.2f}")
+        if self.save_current_model_path:
+            trainer.save_model(self.save_current_model_path)
         return trainer
     
     def evaluate_perplexity(self, trainer):
         eval_results = trainer.evaluate()
         return math.exp(eval_results['eval_loss'])
+    
+if __name__ == "__main__":
+    #add cli args
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-r', '--run_name', required=True, help='experiment run name')
+    ap.add_argument('--save_model', required=True, help='save current model path')
+    ap.add_argument('-o', '--output_dir', required=True, help='output directory')
+    ap.add_argument('-m', '--model_checkpoint', required=False, help='model checkpoint')
+    ap.add_argument('-b', '--block_size', required=False, help='block size')
+    ap.add_argument('-d', '--dataloader_drop_last', required=False, help='dataloader drop last')
+    ap.add_argument('-e', '--evaluation_strategy', required=False, help='evaluation strategy')
+    ap.add_argument('-s', '--save_strategy', required=False, help='save strategy')
+    ap.add_argument('-n', '--num_train_epochs', required=False, help='num train epochs')
+    ap.add_argument('-l', '--logging_steps', required=False, help='logging steps')
+    ap.add_argument('-p', '--per_device_train_batch_size', required=False, help='per device train batch size')
+    ap.add_argument('-q', '--per_device_eval_batch_size', required=False, help='per device eval batch size')
+    ap.add_argument('-l', '--learning_rate', required=False, help='learning rate')
+    ap.add_argument('-c', '--lr_scheduler', required=False, help='lr scheduler')
+    ap.add_argument('-w', '--warmup_steps', required=False, help='warmup steps')
+    ap.add_argument('-g', '--gradient_accumulation_steps', required=False, help='gradient accumulation steps')
+    ap.add_argument('-f', '--use_fp16', required=False, help='use fp16')
+    ap.add_argument('-w', '--weight_decay', required=False, help='weight decay')
+    ap.add_argument('-w', '--monitoring_platform', required=True, help='monitoring platform')
+
+    args = vars(ap.parse_args())
+    trainer = TrainModel(**args)
+    trainer.train_model()
